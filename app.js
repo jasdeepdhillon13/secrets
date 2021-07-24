7bb557be63c3c40e8cfe8b233f02e3020a4fadae
jasdeepdhillon13 <jsdhillon3@gmail.com>
2021-07-24 14:58:40 -0700

    Level 2 encryption

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
